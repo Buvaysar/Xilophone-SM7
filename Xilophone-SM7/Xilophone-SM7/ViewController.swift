65ffe4f2977a5e6efeedfbfb7b79dd33a4d9d7db
Mayrbek <buvaysar1988@mail.ru>
2023-04-20 00:30:02 +0300

    Full-fledged

--- Xilophone-SM7/Xilophone-SM7/ViewController.swift
+++ Xilophone-SM7/Xilophone-SM7/ViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+//        print ("button \(sender.currentTitle!) tapped.")
+        playSound(SoundName: sender.currentTitle!)
+        sender.alpha = 0.5 //button's alpha reduced to half
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0            
+        }
+    }
     
-    
+        
+    func playSound(SoundName: String) {
+        guard let path = Bundle.main.path(forResource: SoundName, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
     
     
